61ad38abcbad9db9990e8ddc45b3d489eeb1a21d
jfdrx <48785965+jfdrx@users.noreply.github.com>
2024-02-15 16:24:01 +0100

    Change the name of the assistant to Banksi

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "Banksi";
+export const AI_DESCRIPTION = "Banksi is the friendly AI assistant of the National Bank of Belgium.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant at the National Bank of Belgium. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
